0ae58bef13bbe70ae6558e3b4d64cc326900dc25
nahyoomi <nahyomi2000@gmail.com>
2025-02-26 23:53:52 +0000

    refactor: centralize icons and images

---
+++ src/assets/index.tsx
@@ -0,0 +1,33 @@
+import LogoIcon from './icons/Logo.svg';
+import LoginIcon from './icons/Login.svg';
+import CartIcon from './icons/Cart.svg';
+import GoogleIcon from './icons/Google.svg';
+import FacebookIcon from './icons/Facebook.svg';
+import InstagramIcon from './icons/Instagram.svg';
+
+export const Icons = {
+    Logo: {
+        src: LogoIcon,
+        alt: 'Logo'
+    },
+    Login: {
+        src: LoginIcon,
+        alt: 'Login'
+    },
+    Cart: {
+        src: CartIcon,
+        alt: 'Cart'
+    },
+    Google: {
+        src: GoogleIcon,
+        alt: 'Google'
+    },
+    Facebook: {
+        src: FacebookIcon,
+        alt: 'Facebook'
+    },
+    Instagram: {
+        src: InstagramIcon,
+        alt: 'Instagram'
+    }
+};
